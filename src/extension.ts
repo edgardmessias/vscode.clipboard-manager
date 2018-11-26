2472f472d213c5a30b8e2007e38d13658b5c0779
Edgard Lorraine Messias <edgardmessias@gmail.com>
2018-11-26 16:22:03 -0200

    Check the clipboard is available

--- src/extension.ts
+++ src/extension.ts
@@ -12,15 +12,32 @@ import { ClipboardTreeDataProvider } from "./tree/history";
 let manager: ClipboardManager;
 
 // this method is called when your extension is activated
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
   const disposable: vscode.Disposable[] = [];
+
+  // Check the clipboard is working
+  try {
+    await defaultClipboard.readText(); // Read test
+  } catch (error) {
+    console.log(error);
+    if (error.message) {
+      vscode.window.showErrorMessage(error.message);
+    } else {
+      vscode.window.showErrorMessage(
+        "Failed to read value from clipboard, check the console log"
+      );
+    }
+    // Disable clipboard listening
+    defaultClipboard.dispose();
+    return;
+  }
 
   // Add to disposable list the default clipboard
   disposable.push(defaultClipboard);
 
   const config = vscode.workspace.getConfiguration("clipboard-manager");
 
-  manager = new ClipboardManager(context);
+  manager = new ClipboardManager(context, defaultClipboard);
   disposable.push(manager);
 
   disposable.push(new PickAndPasteCommand(manager));
