0fc7ae57a526a88a4066c0bbfa147dce3f136ccc
Edgard Lorraine Messias <edgardmessias@gmail.com>
2018-12-05 11:58:36 -0200

    Added AppVeyor test report

--- src/test/index.ts
+++ src/test/index.ts
@@ -8,12 +8,20 @@ import * as IstanbulTestRunner from "./istanbultestrunner";
 
 const testRunner = IstanbulTestRunner;
 
+const mochaOpts: Mocha.MochaOptions = {
+  ui: "tdd", // the TDD UI is being used in extension.test.ts (suite, test, etc.)
+  useColors: true // colored output from test results,
+};
+
+// Enable appveyor reporter if exists
+try {
+  require.resolve("mocha-appveyor-reporter");
+  mochaOpts.reporter = "mocha-appveyor-reporter";
+  // tslint:disable-next-line:no-empty
+} catch (error) {}
+
 testRunner.configure(
-  // Mocha Options
-  {
-    ui: "tdd", // the TDD UI is being used in extension.test.ts (suite, test, etc.)
-    useColors: true // colored output from test results
-  },
+  mochaOpts,
   // Coverage configuration options
   {
     coverConfig: "../../coverconfig.json"
