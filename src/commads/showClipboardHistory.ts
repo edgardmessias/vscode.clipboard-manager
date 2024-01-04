9171524afd9288891a7cb81ced54fa7ea2313580
david yang <david.g.yang@gmail.com>
2024-01-04 15:27:55 -0500

    more lint fixes

--- src/commads/showClipboardHistory.ts
+++ src/commads/showClipboardHistory.ts
@@ -4,27 +4,25 @@ import { commandList } from "./common";
 export class ClipboardHistoryProvider
   implements vscode.TextDocumentContentProvider
 {
-  constructor(protected _manager: ClipboardManager) {
-  }
+  constructor(protected _manager: ClipboardManager) {}
 
-  protected createClipsString(clips: IClipboardItem[]): string  {
+  protected createClipsString(clips: IClipboardItem[]): string {
     return clips
       .map(clip => {
         const createdAt = new Date(clip.createdAt).toLocaleString();
-        const language = clip.language ? `${clip.language}` : '';
+        const language = clip.language ? `${clip.language}` : "";
         const statusLine = `${createdAt} - ${language} - ${clip.createdLocation?.uri.toString()}`;
         const clipText = clip.value.trim();
         const dashLine = "-".repeat(statusLine.length);
         return `${statusLine}\n${dashLine}\n${clipText}`;
       })
-      .join('\n\n');
+      .join("\n\n");
   }
 
   public provideTextDocumentContent(_uri: vscode.Uri): string {
     return this.createClipsString(this._manager.clips);
   }
 }
-
 
 export class ShowClipboardHistory implements vscode.Disposable {
   private _disposable: vscode.Disposable[] = [];
@@ -42,15 +40,15 @@ export class ShowClipboardHistory implements vscode.Disposable {
   protected async execute() {
     const timestamp = Date.now();
     const uri = vscode.Uri.parse(
-      `clipboard-history://history/clipboard-buffer.txt?${timestamp}`);
+      `clipboard-history://history/clipboard-buffer.txt?${timestamp}`
+    );
 
     const document = await vscode.workspace.openTextDocument(uri);
 
     await vscode.window.showTextDocument(document, {
       preview: true,
-      viewColumn: vscode.ViewColumn.Active
+      viewColumn: vscode.ViewColumn.Active,
     });
-
   }
 
   public dispose() {
